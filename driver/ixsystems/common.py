81283010efbd1525385f9e0ca9d039b1815d5d5c
databunnysg <yi@databunny.sg>
2023-02-21 20:36:00 +0000

    feb3

--- driver/ixsystems/common.py
+++ driver/ixsystems/common.py
@@ -340,11 +340,13 @@ class TrueNASCommon(object):
             temp, snapvol = fullvolume.rsplit('/', 1)
             self._delete_snapshot(snapname, snapvol)
 
-        # When deleting volume with dependent snapsnot clone, 
+        # When deleting volume with dependent snapsnot clone,
         # 422 error triggered. Throw VolumeIsBus exception ensures
         # upper stream cinder manager mark volume status available instead of error-deleting.
         if ret['status'] == 'error' and ret['response'] == '422:Unprocessable Entity':
-            errorexception = exception.VolumeIsBusy(_("Cannot delete volume when clone child volume or snapshot exists!"), volume_name=name)
+            errorexception = exception.VolumeIsBusy(
+                _("Cannot delete volume when clone child volume or snapshot exists!")
+                , volume_name=name)
             raise errorexception
         elif ret['status'] != FreeNASServer.STATUS_OK:
             msg = ('Error while deleting volume: %s' % ret['response'])
